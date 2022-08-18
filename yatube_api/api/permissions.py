f9f3602709693a7a53382d1ca56d0a937de0d286
anna kartasheva <annakartashevamail@gmail.com>
2022-08-18 17:47:36 +0300

    final

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,8 +7,7 @@ class AuthorOrReadOnly(permissions.BasePermission):
                 or request.method in permissions.SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
 
 
@@ -22,6 +21,5 @@ class ReadOnly(permissions.BasePermission):
 
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author != request.user)

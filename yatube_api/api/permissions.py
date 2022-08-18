9623e4317da905c959e6247490322f9247ac16cf
anna kartasheva <annakartashevamail@gmail.com>
2022-08-18 17:35:36 +0300

    and another

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,13 +4,13 @@ from rest_framework import permissions
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS or
-                request.user.is_authenticated)
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS or obj.author
-                == request.user)
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
 
 
 class ReadOnly(permissions.BasePermission):
@@ -24,5 +24,5 @@ class ReadOnly(permissions.BasePermission):
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS or obj.author !=
-                request.user)
+                request.method in permissions.SAFE_METHODS
+                or obj.author != request.user)

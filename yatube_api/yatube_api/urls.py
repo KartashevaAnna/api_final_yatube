777551ef19d3e8eedfd29396756729235b512617
anna kartasheva <annakartashevamail@gmail.com>
2022-08-18 17:14:59 +0300

    fixed urls pep8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -17,12 +17,11 @@ urlpatterns = [
 ]
 
 schema_view = get_schema_view(
-   openapi.Info(
+    openapi.Info(
       title="Cats API",
       default_version='v1',
-      description="Документация для приложения cats проекта Kittygram",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="admin@kittygram.ru"),
+      description="Документация для приложения api проекта Yatube",
+      contact=openapi.Contact(email="annakartashevamail@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
     public=True,

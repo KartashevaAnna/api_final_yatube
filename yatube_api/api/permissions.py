00f2569945d6a177aa0440636b9383a0778b63ef
anna kartasheva <annakartashevamail@gmail.com>
2022-08-18 16:47:47 +0300

    line continuation fixed

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,11 +4,11 @@ from rest_framework import permissions
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS \
-               or request.user.is_authenticated
+        or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS \
-               or obj.author == request.user
+        or obj.author == request.user
 
 
 class ReadOnly(permissions.BasePermission):
@@ -22,4 +22,4 @@ class ReadOnly(permissions.BasePermission):
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS \
-               or obj.author != request.user
+        or obj.author != request.user

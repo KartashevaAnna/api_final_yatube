d4543bda7cdfc9e922ea9a2b3c073453b9e5a458
anna kartasheva <annakartashevamail@gmail.com>
2022-08-18 17:11:57 +0300

    part of idents fixed

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,12 +3,16 @@ from rest_framework import permissions
 
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS \
-        or request.user.is_authenticated
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS \
-        or obj.author == request.user
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+        )
 
 
 class ReadOnly(permissions.BasePermission):
@@ -21,5 +25,7 @@ class ReadOnly(permissions.BasePermission):
 
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS \
-        or obj.author != request.user
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author != request.user
+        )

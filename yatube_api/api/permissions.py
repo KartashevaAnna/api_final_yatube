09fb7b956935941084133e8d1b1b6ef9ad062e2b
anna kartasheva <annakartashevamail@gmail.com>
2022-08-22 11:50:19 +0300

    changed permissions formatting

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,12 +3,18 @@ from rest_framework import permissions
 
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                or request.method in permissions.SAFE_METHODS)
+        return (
+                request.user.is_authenticated
+                or
+                request.method in permissions.SAFE_METHODS
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        return (
+                request.method in permissions.SAFE_METHODS
+                or
+                obj.author == request.user
+                )
 
 
 class ReadOnly(permissions.BasePermission):
@@ -21,5 +27,8 @@ class ReadOnly(permissions.BasePermission):
 
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author != request.user)
+        return (
+                request.method in permissions.SAFE_METHODS
+                or
+                obj.author != request.user
+                )

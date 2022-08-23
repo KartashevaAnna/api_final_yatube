a37471b6a581505e65ac11b60864a7d1f2f2bdd7
anna kartasheva <annakartashevamail@gmail.com>
2022-08-23 11:31:17 +0300

    pep8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,18 +3,14 @@ from rest_framework import permissions
 
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                or request.method in permissions.SAFE_METHODS
-                )
-    #     Я хотела написать как в закомментированном коде, такой пример давал
-    #      наставник. Это было бы удобнее для чтения и больше похоже на
-    #     требования PEP8. Но автотесты Яндекс.Практикума такой вариант
-    # не пропускают.
-        # return (
-        #         request.user.is_authenticated
-        #         or
-        #         request.method in permissions.SAFE_METHODS
-        # )
+        # return (request.method in permissions.SAFE_METHODS
+        #         or request.user.is_authenticated
+        #         )
+        return (
+                request.method in permissions.SAFE_METHODS
+                or
+                request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS

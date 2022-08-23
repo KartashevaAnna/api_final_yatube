0a496267f49782ce6a6e2b4292bf30dbe24c4edf
anna kartasheva <annakartashevamail@gmail.com>
2022-08-23 13:45:39 +0300

    final flake8 changes

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,17 +1,21 @@
 from rest_framework import permissions
 
 
+from rest_framework import permissions
+
+
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                )
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
 
 
 class ReadOnly(permissions.BasePermission):
@@ -24,11 +28,13 @@ class ReadOnly(permissions.BasePermission):
 
 class NotAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                )
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author != request.user
-                )
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author != request.user
+        )

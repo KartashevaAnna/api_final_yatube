751da6cc8218c62355596ee69b5ba7d98fdf12ef
Anna Kartasheva <annakartashevamail@gmail.com>
2023-02-02 17:16:30 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -5,7 +5,9 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 DEBUG = True
 
